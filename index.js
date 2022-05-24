86a546acefd795edab5a7ff3d076fa0aef16624f
Milos Milijasevic <milosrm20005@gmail.com>
2022-05-24 09:10:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+
+const inputBill = document.querySelector(`.bill`);
+const inputTip = document.querySelector(`.tip`);
+const inputTotal = document.querySelector(`.total`);
+const buttonCalculate = document.querySelector(`.calculate`);
