d7087dc77b33eff3e3ce60dd2c715e7eb426b95c
Milos Milijasevic <milosrm20005@gmail.com>
2022-05-25 09:20:21 +0200

    js updated

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ const inputBill = document.querySelector(`.bill`);
 const inputTip = document.querySelector(`.tip`);
 const inputTotal = document.querySelector(`.total`);
 const buttonCalculate = document.querySelector(`.calculate`);
+
+
+buttonCalculate.addEventListener(`click`, () =>{
+
+    calculateFunction();
+
+});
+
+inputBill.addEventListener(`keyup`, (e) =>{
+
+    if(e.keyCode === 13){
+
+        calculateFunction();
+
+    }
+
+});
+
+
+const calculateFunction = () =>{
+
+    if(inputBill.value > 0){
+
+        inputTip.value = inputBill.value * 0.15;
+        
+        inputTotal.value = Number(inputTip.value) + Number(inputBill.value);
+
+    }
+
+};
